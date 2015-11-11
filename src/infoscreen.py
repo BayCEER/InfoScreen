7dc9f46cc540a0be53ac480713d52cdaeda69a7d
oarchner <oliver.archner@uni-bayreuth.de>
2015-11-11 16:59:13 +0100

    BayCEER Homepage

--- src/infoscreen.py
+++ src/infoscreen.py
@@ -1,19 +1,21 @@
-'''
-Created on 09.11.2015
+#!/usr/bin/python
 
-@author: oliver
-'''
 import sys
 import platform
 import logging
 import os
 import datetime
 import time
+import json
 
+from PyQt4.QtWebKit import QWebView
 from PyQt4.QtGui import QApplication
-from PyQt4.Qt import  QWebView, QUrl
+from PyQt4.QtCore import QUrl
 from PyQt4.QtCore import QThread
 from PyQt4.QtWebKit import QWebSettings
+from PyQt4.QtGui import QCursor
+from PyQt4.QtCore import Qt
+from PyQt4.QtCore import SIGNAL
 
 from logging.handlers import RotatingFileHandler
 
@@ -24,7 +26,7 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 if platform.system() == "Linux":
-    fh = RotatingFileHandler("/var/log/infoscreen.log", maxBytes=2048, backupCount=10)
+    fh = RotatingFileHandler("/var/log/infoscreen.log", maxBytes=4096, backupCount=10)
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
@@ -44,25 +46,51 @@ def readConfig(path):
     
 
 class MainView(QWebView):
-    def __init__(self, page, parent=None):
+    def __init__(self, page, secs, parent=None):
         super(MainView, self).__init__(parent)
         self.settings().setAttribute(QWebSettings.PluginsEnabled, True)
-        self.settings().setAttribute(QWebSettings.PrivateBrowsingEnabled, True)
-        self.settings().setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
-        self.load(QUrl(page))
+        # self.settings().setAttribute(QWebSettings.PrivateBrowsingEnabled, True)
+        # self.settings().setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
+        # self.settings().setAttribute(QWebSettings.DeveloperExtrasEnabled, True)        
         self.loadFinished.connect(self._loadFinished)
-        self.loadStarted.connect(self._loadStarted)               
+        self.loadStarted.connect(self._loadStarted)
+        self.load(QUrl(page))               
         
         if platform.system() == "Linux":
             self.screenSaver = ScreenSaver()
-            self.screenSaver.start()        
+            self.screenSaver.start()
+        
+        self.worker = ReloadThread(secs)
+        self.connect(self.worker, SIGNAL("reload"), self.reloadPage)
+        self.worker.start()        
     
     def _loadStarted(self):      
         self.loading = True
     
     def _loadFinished(self, ok):        
         self.loaded = ok
-        self.loading = False    
+        if self.loaded!=True:
+            logger.warning("Failed to load page.")            
+        self.loading = False
+        
+    def reloadPage(self):
+        logger.info("Reload page")
+        self.reload()
+    
+    
+
+class ReloadThread(QThread):                 
+    def __init__(self, secs):
+        QThread.__init__(self)
+        self._secs = secs
+        self._stopped = False                
+    def __del__(self):
+        self._stopped = True                                    
+    def run(self):
+        while not self._stopped:
+            time.sleep(self._secs)
+            self.emit(SIGNAL("reload"))
+               
 
 class ScreenSaver(QThread):
     def __init__(self):
@@ -84,20 +112,37 @@ class ScreenSaver(QThread):
                 os.system("xset dpms force on")
             time.sleep(60)          
 
+def printUsage():
+    print("Usage: infoscreen <url> <reloadIonterval>")
+    sys.exit(-1)
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
+    logger.info("Application started.")
     app = QApplication(sys.argv)
     
-    if (len(sys.argv)<2):
-        if platform.system() == "Linux":
-            startPage = readConfig("/etc/infoscreen/infoscreen.json")["startPage"]
-        else:
-            print("Usage: infoscreen <url>")
-            sys.exit(-1)        
-    else:
-        startPage = sys.argv[1]
+    if platform.system() == "Linux":
+        app.setOverrideCursor(QCursor(Qt.BlankCursor)) 
     
-    main = MainView(startPage)            
-    main.showFullScreen();    
+    
+    if (len(sys.argv)<3):
+        if platform.system() == "Linux":
+            config = readConfig("/etc/infoscreen/infoscreen.json")                            
+            startPage = config["startPage"]
+            reloadInterval = config["reloadInterval"]
+        else:
+            print("Usage: infoscreen <url> <reloadIonterval>")
+            sys.exit(-1)        
+    else:        
+        startPage = sys.argv[1]
+        reloadInterval = sys.argv[2]
+
+    logger.info("Loading start page:{}".format(startPage))
+    main = MainView(startPage,float(reloadInterval))  
+    
+    if platform.system() == "Linux":          
+        main.showFullScreen();
+    else:
+        main.showMaximized()    
+    
     sys.exit(app.exec_())
     
